3b938fb0629fda3bfdd0a3039570e4d2a9f89de3
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-02-17 12:33:51 +0600

    function added

--- JS/app.js
+++ JS/app.js
@@ -1,47 +1,128 @@
-// handle calculate button
-document.getElementById('calculate-button').addEventListener('click', function () {
-    //user total income handle
-    
-    const userTotalIncome = document.getElementById('user-income-input');
-    const newtotalIncomeText = userTotalIncome.value;
-    const newTotalIncome = parseFloat(newtotalIncomeText);
-    
-    //user food amount handle
-    const userFoodCostInput = document.getElementById('user-food-expenses');
-    const newFoodAmountText = userFoodCostInput.value;
-    const newFoodAmount = parseFloat(newFoodAmountText);
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const inputAmountValue = parseFloat(inputAmountText);
 
-    //user rent amount handle
-    const userRentCostInput = document.getElementById('user-rent-expenses');
-    const newRentAmountText = userRentCostInput.value;
-    const newRentAmount = parseFloat(newRentAmountText);
-
-    //user clothes amount handle
-    const userClothsCostInput = document.getElementById('user-clothes-expenses');
-    const newClothesAmountText = userClothsCostInput.value;
-    const newClothesAmount = parseFloat(newClothesAmountText);
-
-    //update total expances 
+    //clear input field
+    inputField.value = '';
+    return inputAmountValue;
+}
+function getCurrentTotalExpenses() {
     const userTotalExpense = document.getElementById('total-expense');
     const userTotalExpenseText = userTotalExpense.innerText;
     const previousTotalExpense = parseFloat(userTotalExpenseText);
-    const newTotalExpenses = previousTotalExpense + newClothesAmount + newRentAmount + newFoodAmount;
-
-    userTotalExpense.innerText = newTotalExpenses;
-
-    //update balance 
+    return previousTotalExpense;
+}
+function getPreviousBalance() {
     const userBalance = document.getElementById('total-balance');
-    const userBalenceText = userBalance.innerText;
-    const previousTotalBalance = parseFloat(userBalenceText);
-    const newUserBalance = ((previousTotalBalance + newTotalIncome) - newTotalExpenses);
+    const userTotalExpenseText = userBalance.innerText;
+    const previousTotalExpense = parseFloat(userTotalExpenseText);
+    return previousTotalExpense;
+}
+function updateCurrentTotalExpenses(foodAmount, rentAmount, clothesAmount, isAdd) {
+    const userTotalExpense = document.getElementById('total-expense');
+    const previousTotalExpense = getCurrentTotalExpenses();
+    if (isAdd == true) {
+        userTotalExpense.innerText = previousTotalExpense + foodAmount + rentAmount + clothesAmount;
+    }
+    const totalExpenses = userTotalExpense.innerText
+    return totalExpenses;
+}
 
-    userBalance.innerText = newUserBalance
+function userCurrentBalance(incomeAmount, isDone) {
+    const userBalance = document.getElementById('total-balance');
+    const userTotalIncome = getInputValue('user-income-input');
+    const previousBalance = getPreviousBalance();
+    const userCalculatedTotalExpence = updateCurrentTotalExpenses();
+    if(isDone == true){
+        userBalance.innerText = ((previousBalance + incomeAmount) - userCalculatedTotalExpence);
+    }
+    const userBalanceNew = userBalance.innerText;
+    return userBalanceNew;
+}
 
-    //clear all the input field
-    userTotalIncome.value = '';
-    userFoodCostInput.value = '';
-    userRentCostInput.value = '';
-    userClothsCostInput.value = '';
+//Error Message
+function showErrorMessage() {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = "Please Enter a Positive Number!!!"
+}
+
+//Error Message for String
+function showErrorMessageForString() {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = "You have entered a String. Please enter a Positive Number!!!"
+}
+
+// handle calculate button
+document.getElementById('calculate-button').addEventListener('click', function () {
+    //user total income handle
+
+    const userTotalIncome = getInputValue('user-income-input');
+    const userFoodCost = getInputValue('user-food-expenses');
+    const userRentCost = getInputValue('user-rent-expenses');
+    const userClothsCost = getInputValue('user-clothes-expenses');
+
+    if (userFoodCost > 0 && userRentCost > 0 && userClothsCost > 0) {
+        updateCurrentTotalExpenses(userFoodCost, userRentCost, userClothsCost, true);
+    }
+    else if (isNaN(userFoodCost) || isNaN(userRentCost) || isNaN(userClothsCost)) {
+        showErrorMessageForString();
+    }
+    else {
+        showErrorMessage();
+    }
+    if (userTotalIncome > 0){
+        userCurrentBalance(userTotalIncome, true);
+    }
+    else if (isNaN(userTotalIncome)) {
+        showErrorMessageForString();
+    }
+    else {
+        showErrorMessage();
+    }
+
+    // const userBalance = document.getElementById('total-balance');
+    // const previousBalance = getPreviousBalance();
+    // const userCalculatedTotalExpence = updateCurrentTotalExpenses();
+    // const calculatedBalance = ((previousBalance + userTotalIncome) - userCalculatedTotalExpence);
+    // userBalance.innerText = calculatedBalance;
+
+    // //user food amount handle
+    // const userFoodCostInput = document.getElementById('user-food-expenses');
+    // const newFoodAmountText = userFoodCostInput.value;
+    // const newFoodAmount = parseFloat(newFoodAmountText);
+
+    // //user rent amount handle
+    // const userRentCostInput = document.getElementById('user-rent-expenses');
+    // const newRentAmountText = userRentCostInput.value;
+    // const newRentAmount = parseFloat(newRentAmountText);
+
+    // //user clothes amount handle
+    // const userClothsCostInput = document.getElementById('user-clothes-expenses');
+    // const newClothesAmountText = userClothsCostInput.value;
+    // const newClothesAmount = parseFloat(newClothesAmountText);
+
+    // //update total expances 
+    // const userTotalExpense = document.getElementById('total-expense');
+    // const userTotalExpenseText = userTotalExpense.innerText;
+    // const previousTotalExpense = parseFloat(userTotalExpenseText);
+    // const newTotalExpenses = previousTotalExpense + newClothesAmount + newRentAmount + newFoodAmount;
+
+    // userTotalExpense.innerText = newTotalExpenses;
+
+    // //update balance 
+    // const userBalance = document.getElementById('total-balance');
+    // const userBalenceText = userBalance.innerText;
+    // const previousTotalBalance = parseFloat(userBalenceText);
+    // const newUserBalance = ((previousTotalBalance + newTotalIncome) - newTotalExpenses);
+
+    // userBalance.innerText = newUserBalance
+
+    // //clear all the input field
+    // userTotalIncome.value = '';
+    // userFoodCostInput.value = '';
+    // userRentCostInput.value = '';
+    // userClothsCostInput.value = '';
 
     //handle the save button
     document.getElementById('save-button').addEventListener('click', function () {
@@ -70,4 +151,5 @@ document.getElementById('calculate-button').addEventListener('click', function (
         userSaveingParsentageAmount.value = '';
     });
 });
+
 
